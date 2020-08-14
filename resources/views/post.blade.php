20cbe5491dcb153349b1e37f0f0c42d04996c976
praveen8git <omniplayer.cosmo9@gmail.com>
2020-08-15 01:11:13 +0530

    minor changes

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1>Post</h1>
+    <form action="" method="post">
+        <p>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title">
+        </p>
+        <p>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        </p>
+        <p><input type="submit" value="Post"></p>
+    </form>
+</body>
+</html>
